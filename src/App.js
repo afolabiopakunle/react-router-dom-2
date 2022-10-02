5efe86a05a68fd11a5b278f63fb0847c53892de4
afolabiopakunle <opakunleafolabi@gmail.com>
2022-10-02 09:32:33 +0100

    Links implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home'
 import BookList from './pages/BookList'
 import "./style.css";
 
 export default function App() {
   return (
+    <>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/books'>Books</Link></li>
+      </ul>
+      </nav>
    <Routes>
      <Route path='/' element={<Home />}/>
      <Route path='/books' element={<BookList />}/>
      </Routes>
+     </>
   );
 }
